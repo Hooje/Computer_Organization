d2ec07995dd9efcf66082f7f600dc24f2e6359a8
urocissa <t1213121.cs07@nctu.edu.tw>
2020-05-10 11:54:43 +0800

    change with problem on slt

--- ALU.v
+++ ALU.v
@@ -16,82 +16,30 @@ module ALU(
 	output          zero_o;
 
 	reg    [32-1:0] result_o;   //may be use another wire to connect
-	reg             zero_o;
+	reg             zero_o;	
 
-	reg							A_invert;
-	reg							B_invert;
-	reg							cin;
-	reg				[1:0]	operation;
-
-	reg							less;
-	reg							A_sign;
-	reg							B_sign;
-	reg							diff;
-	reg							bus;
-	wire			[31:0]C;
-	wire			[31:0]outcome;
-	
-	ALUtop AluTop (src1_i[0],src2_i[0],less,A_invert,B_invert,cin,operation,outcome[0],C[0]);
-	
-	genvar i;
-	generate
-	for(i=1; i<32; i=i+1)
-		begin: gen_loop
-			ALUsingle Alu_unit (src1_i[i],src2_i[i],A_invert,B_invert,C[i-1],operation,outcome[i],C[i]);
-		end
-	endgenerate
-
-	always@(*)
+always@(*)
 	begin
-			cin <= 0;
-			less <= result_o[31];
-
-			bus <= |result_o;
-			zero_o <= ~bus;
-
 			case(ctrl_i)
 				4'b0000:begin //bitwise AND
-					A_invert <= 0;
-					B_invert <= 0;
-					operation <= 2'b00;
+					result_o = src1_i & src2_i;
 				end
 				4'b0001:begin //bitwise OR
-					A_invert <= 0;
-					B_invert <= 0;
-					operation <= 2'b01;
+					result_o = src1_i | src2_i;	
 				end
 				4'b0010:begin //Addition
-					A_invert <= 0;
-					B_invert <= 0;
-					operation <= 2'b10;
+					result_o = src1_i + src2_i;
 				end
 				4'b0110:begin //Subtraction
-					A_invert <= 0;
-					B_invert <= 1;
-					operation <= 2'b10;
-					cin <= 1;
+					result_o = src1_i - src2_i;
+					zero_o = result_o == 0 ? 1 : 0;
 				end
 				4'b0111:begin //set less than
-					A_invert <= 0;
-					B_invert <= 1;
-					operation <= 2'b11;
-				end
-				4'b1100:begin //bitwise NOR
-					A_invert <= 1;
-					B_invert <= 1;
-					operation <= 2'b00;
-				end
-				4'b1101:begin //bitwise NAND
-					A_invert <= 1;
-					B_invert <= 1;
-					operation <= 2'b01;
+					result_o = src1_i < src2_i ? 1 : 0;
 				end
 				default:begin
 				end
 			endcase
-			
-			result_o <= outcome;
-
 	end
 
 endmodule
