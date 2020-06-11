46b7a688fee91bcd1d427afd178b52b573baee5c
urocissa <t1213121.cs07@nctu.edu.tw>
2020-06-11 21:00:57 +0800

    include upload file

--- Lab4/direct_mapped_cache_lru.cpp
+++ Lab4/direct_mapped_cache_lru.cpp
@@ -47,8 +47,10 @@ bool insert_cache(cache_content *set, unsigned int tag, int way)
 		if(set[i].tag == tag)
 		{
 			temp = set[i];
-			for(j=i; j>=1; j--)
-				set[j] = set[j-1];
+			for(j=i; j>=1; j--){
+				set[j].tag = set[j-1].tag;
+				set[j].v = set[j-1].v;
+			}
 			set[0] = temp;
 			return true;
 		}
@@ -56,8 +58,10 @@ bool insert_cache(cache_content *set, unsigned int tag, int way)
 	
 	if(valid == way){	//condition of full
 	
-		for(j = way - 1; j >= 1; j--)
-			set[j] = set[j-1];
+		for(j = way - 1; j >= 1; j--){
+			set[j].tag = set[j-1].tag;
+			set[j].v = set[j-1].v;
+		}
 
 		set[0].tag = tag;
 		set[0].v = true;
@@ -66,8 +70,10 @@ bool insert_cache(cache_content *set, unsigned int tag, int way)
 		
 	}else{	//condition of not full
 		
-		for(j = valid; j >= 1; j--)
-			set[j] = set[j-1];
+		for(j = valid; j >= 1; j--){
+			set[j].tag = set[j-1].tag;
+			set[j].v = set[j-1].v;
+		}
 
 		set[0].tag = tag;
 		set[0].v = true;
